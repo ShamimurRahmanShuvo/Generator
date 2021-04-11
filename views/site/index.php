2804c5d41746db14016cf75897e8e2eb050ca8c4
Md Shamimur Rahman Shuvo <72037286+ShamimurRahmanShuvo@users.noreply.github.com>
2021-04-11 08:44:05 -0400

    Add files via upload

---
+++ views/site/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Nourish Poultry';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome!</h1>
+
+        <p class="lead">To our Generator dashboard.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://sgcsoft.net/generator/web/index.php?r=site%2Flogin">Login</a> to continue</p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+
+        </div>
+
+    </div>
+</div>
---
+++ views/site/indexi.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Nourish Poultry';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome!</h1>
+
+        <p class="lead">To our Generator dashboard.</p>
+
+        <p>You have successfully Logged in</p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+
+        </div>
+
+    </div>
+</div>
